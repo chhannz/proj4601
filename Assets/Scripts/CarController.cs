05a4ec195aaafe6a511a6975a7407d7050a02942
chhannz <chhannz@gmail.com>
2026-06-17 15:03:12 +0700

    17juni

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private CarData carData;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Controller controls;
+    private Vector2 moveInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
